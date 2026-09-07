11540005f7e5056a317a88d968e2255e10c07640
tecniredperu <tecniredpe@gmail.com>
2026-09-07 10:03:05 -0500

    Transferencias: ruta nueva

---
+++ src/app/transferencias/nueva/page.tsx
@@ -0,0 +1,5 @@
+import { AppShell } from "@/components/layout/app-shell";
+import { TransferForm } from "@/modules/transfers/transfer-form";
+import { getTransferOptions } from "@/modules/transfers/transfers.repository";
+export const dynamic="force-dynamic";
+export default async function NewTransferPage(){const data=await getTransferOptions();return <AppShell><TransferForm {...data}/></AppShell>}
