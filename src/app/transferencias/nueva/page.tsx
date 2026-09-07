df59662efecb9b27c3aaa702daf1b5d4f2aaf573
tecniredperu <tecniredpe@gmail.com>
2026-09-07 12:09:43 -0500

    Permisos: proteger nueva transferencia

--- src/app/transferencias/nueva/page.tsx
+++ src/app/transferencias/nueva/page.tsx
@@ -1,5 +1,6 @@
 import { AppShell } from "@/components/layout/app-shell";
+import { requirePermission } from "@/lib/business-context";
 import { TransferForm } from "@/modules/transfers/transfer-form";
 import { getTransferOptions } from "@/modules/transfers/transfers.repository";
 export const dynamic="force-dynamic";
-export default async function NewTransferPage(){const data=await getTransferOptions();return <AppShell><TransferForm {...data}/></AppShell>}
+export default async function NewTransferPage(){await requirePermission("inventory.transfer");const data=await getTransferOptions();return <AppShell><TransferForm {...data}/></AppShell>}
