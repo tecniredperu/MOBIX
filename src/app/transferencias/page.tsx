aeec020a144a2390f9d0d59bd3962ce6617978b1
tecniredperu <tecniredpe@gmail.com>
2026-09-07 12:09:31 -0500

    Permisos: proteger transferencias

--- src/app/transferencias/page.tsx
+++ src/app/transferencias/page.tsx
@@ -1,5 +1,6 @@
 import { AppShell } from "@/components/layout/app-shell";
+import { requirePermission } from "@/lib/business-context";
 import { TransfersView } from "@/modules/transfers/transfers-view";
 import { getTransfers } from "@/modules/transfers/transfers.repository";
 export const dynamic="force-dynamic";
-export default async function TransfersPage(){return <AppShell><TransfersView items={await getTransfers()}/></AppShell>}
+export default async function TransfersPage(){await requirePermission("inventory.transfer");return <AppShell><TransfersView items={await getTransfers()}/></AppShell>}
